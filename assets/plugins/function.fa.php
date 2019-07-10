35860906036540f578362a90c7ebe6e619acff6a
Stephen <stephen@millipedia.co.uk>
2019-07-10 11:54:08 +0100

    Added help text.

--- assets/plugins/function.fa.php
+++ assets/plugins/function.fa.php
@@ -1,6 +1,6 @@
 <?php
 // A simple function to embed Font^H^HrkAwesome icons on the page
-// Simply outputs the appropriate <i> element and assumes you have already inluded / linked the css 
+// Simply outputs the appropriate <i> element and assumes you have already linked the css 
 //
 // Drafted by Calguy1000 in IRC
 // tweaked very slightly by stephen@millipedia.
@@ -14,5 +14,26 @@
 function smarty_function_fa($params, &$smarty) {
 	$icon = get_parameter_value($params,'i');
 	$icon = get_parameter_value($params,'icon',"fa-".$icon);
-	return  "<i class=\"fa {$icon}\"  aria-hidden=\"true\"></i> ";
+	return  "<i class=\"fa {$icon}\" aria-hidden=\"true\"></i> ";
+}
+
+
+
+function smarty_cms_help_function_fa() {
+	?>
+	<h3>What does this do?</h3>
+	<p>
+    A simple function to embed FontAwesome icons on the page<br>
+    Simply outputs the appropriate <i> element and assumes you have already linked the css<br>
+    <br>
+    Drafted by Calguy1000 in IRC<br>
+    tweaked very slightly by stephen@millipedia.<br>
+    <br>
+    Use i param for shorthand<br>
+    Use icon param for full/spaces<br>
+    Examples:<br>
+            {fa i=cube}<br>
+            {fa icon="fa-cube fa-lg"}<br>
+    </p>
+	<?php
 }
